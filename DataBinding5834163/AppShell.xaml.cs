9afac033c7de22f6088c6d4bccfa6967b7cb05b0
EmelyNj <23emelynajera@gmail.com>
2024-07-15 13:17:43 -0600

    Agregar archivos de proyecto.

---
+++ DataBinding5834163/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DataBinding5834163
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DataBinding5834163/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace DataBinding5834163
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+
+}
